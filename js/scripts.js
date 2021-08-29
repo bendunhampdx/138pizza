0d1225053ae8e325b2936ab152c259c4b34d808f
bendunhampdx <bendunhampdx@gmail.com>
2021-08-29 14:44:58 -0700

    create order constructor and price prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
 // Business Logic
 
+function Order(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+  this.price = 0;
+}
 
-
-
-
-
-
-
-
-
-
+Order.prototype.price = function() {
+  if (this.size === "large") {
+    this.price = 20;
+  } else if (this.size === 'medium') {
+    this.price = 15;
+  } else if (this.size === 'small') {
+    this.price = 12;
+  } else {
+    this.price = 0;
+  }
+}
 
 
 
@@ -27,6 +34,6 @@
 $(document).ready(function() {
   $("form#order").submit(function(event) {
     event.preventDefault();
-    
+
   })
 })
