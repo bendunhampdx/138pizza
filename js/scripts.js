e5c90bf0ca46368834a833ff0b46a9ed6c36d195
bendunhampdx <bendunhampdx@gmail.com>
2021-08-29 14:36:31 -0700

    add JS UI template, and submit button

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// Business Logic
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// User Interface Logic
+
+$(document).ready(function() {
+  $("form#order").submit(function(event) {
+    event.preventDefault();
+    
+  })
+})
